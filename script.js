4cdf1b220ec30925a544d3574fa7725920330240
Swapnil Thawale <92202649+Swapnil213@users.noreply.github.com>
2025-03-24 22:38:50 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -3,14 +3,21 @@ let checkBtn = document.querySelector("#check-btn");
 let generateOtpForm = document.querySelector(".otpform");
 let checkOtpForm = document.querySelector(".checkform");
 let userOtp = document.querySelector("#user-otp");
+let userNumber = document.querySelector("#number");
 let otp = 0;
 
 btn.addEventListener("click", () => {
-    otp = Math.floor(Math.random() * 10000);
-    console.log(otp);
-    alert("OTP Generated");
-    generateOtpForm.style.display = "none";
-    checkOtpForm.style.display = "flex";
+    console.log(userNumber.value.length);
+    if (userNumber.value.length == 10) {
+        otp = Math.floor(Math.random() * 10000);
+        console.log(otp);
+        alert("OTP Generated");
+        generateOtpForm.style.display = "none";
+        checkOtpForm.style.display = "flex";
+    }
+    else {
+        alert("Number should be of length 10");
+    }
 });
 
 checkBtn.addEventListener("click", () => {
